c2517b6107c622aecf4d0f82318153a61ad91dfe
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-11-18 22:13:21 +0700

    remove users from protected verifyJWT

--- server.js
+++ server.js
@@ -46,9 +46,9 @@ app.use("/refresh", require("./routes/refresh"));
 app.use("/logout", require("./routes/logout"));
 
 app.use("/rooms", require("./routes/api/rooms"));
+app.use("/users", require("./routes/api/users"));
 app.use(verifyJWT);
 app.use("/employees", require("./routes/api/employees"));
-app.use("/users", require("./routes/api/users"));
 
 app.all("*", (req, res) => {
   res.status(404);
