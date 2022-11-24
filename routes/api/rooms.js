4d63e30d98ac4be87f3bee0f8429c79b35a78deb
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-11-24 13:55:07 +0700

    add multer to add kamar

--- routes/api/rooms.js
+++ routes/api/rooms.js
@@ -3,12 +3,27 @@ const router = express.Router();
 const roomsController = require("../../controllers/roomsController");
 const ROLES_LIST = require("../../config/roles_list");
 const verifyRoles = require("../../middleware/verifyRoles");
+const multer = require("multer");
+const path = require("path");
+
+// config multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "..", "..", "public", "img", "uploads"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router
   .route("/")
   .get(roomsController.getAllRooms)
   .post(
-    verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),
+    // verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),
+    upload.single("image"),
     roomsController.createNewRoom
   )
   .put(
